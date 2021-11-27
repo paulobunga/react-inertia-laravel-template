3af49b02409a5202731e22818ba458e34c58e1f8
Paul Obunga <paulobunga.one@gmail.com>
2021-11-27 12:05:50 +0300

    Updated Code

---
+++ resources/js/app.js
@@ -0,0 +1,12 @@
+// require('./bootstrap');
+
+import { createInertiaApp } from "@inertiajs/inertia-react";
+import React from "react";
+import { render } from "react-dom";
+
+createInertiaApp({
+    resolve: (name) => require(`./Pages/${name}`),
+    setup({ el, App, props }) {
+        render(<App {...props} />, el);
+    },
+});
